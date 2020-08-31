390863386d45e90ddf0db2177b94c6dbd4c3cfb4
Michal Szczepanski <michal@vane.pl>
2020-08-31 17:12:17 +0200

    Update NotesMainView convert component to functional

--- src/view/notes/NotesMainView.jsx
+++ src/view/notes/NotesMainView.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 import NotesAddButton from './NotesAddButton';
 import NotesList from './NotesList';
 
-class NotesMainView extends React.Component {
-  render() {
-    return (
-      <div>
-        <NotesAddButton />
-        <button type="button" onClick={this.handleSayTest}>Say Test</button>
-        <NotesList />
-      </div>
-    );
-  }
-}
+const NotesMainView = () => (
+  <div>
+    <NotesAddButton />
+    <NotesList />
+  </div>
+);
 
 export default NotesMainView;
