ff68f9555c62b47d9a38fe6ee66d3a6889ade89f
Michal Szczepanski <michal@vane.pl>
2020-08-31 23:41:06 +0200

    Update TextEditor add alignment and striketrough

--- src/view/editor/TextEditor.jsx
+++ src/view/editor/TextEditor.jsx
@@ -5,10 +5,16 @@ import {
   faBold,
   faItalic,
   faUnderline,
+  faStrikethrough,
   faListOl,
   faListUl,
+  faAlignCenter,
+  faAlignLeft,
+  faAlignRight,
+  faAlignJustify,
 } from '@fortawesome/free-solid-svg-icons';
 import 'draft-js/dist/Draft.css';
+import './text-editor.css';
 
 const textEditorContainer = {
   width: '100%',
@@ -21,6 +27,29 @@ const textEditorToolbar = {
   width: '100%',
   display: 'flex',
   flexDirection: 'row',
+};
+
+const blockStyleFn = (block) => {
+  const type = block.getType();
+  if (type === 'ALIGN-LEFT') {
+    return 'text-editor-align-left';
+  }
+  if (type === 'ALIGN-CENTER') {
+    return 'text-editor-align-center';
+  }
+  if (type === 'ALIGN-RIGHT') {
+    return 'text-editor-align-right';
+  }
+  if (type === 'ALIGN-JUSTIFY') {
+    return 'text-editor-align-justify';
+  }
+  return null;
+};
+
+const customStyleMap = {
+  STRIKETHROUGH: {
+    textDecoration: 'line-through',
+  },
 };
 
 class TextEditor extends React.Component {
@@ -49,6 +78,13 @@ class TextEditor extends React.Component {
     this.editor.focus();
   }
 
+  handleAlginStyle = (e, styleName) => {
+    e.preventDefault();
+    const { editorState } = this.state;
+    this.handleChange(RichUtils.toggleBlockType(editorState, styleName));
+    this.editor.focus();
+  }
+
   handleChange = (editorState) => {
     this.setState({ editorState });
   }
@@ -67,16 +103,33 @@ class TextEditor extends React.Component {
           <button type="button" onMouseDown={(e) => this.handleInlineStyle(e, 'UNDERLINE')}>
             <FontAwesomeIcon icon={faUnderline} />
           </button>
+          <button type="button" onMouseDown={(e) => this.handleInlineStyle(e, 'STRIKETHROUGH')}>
+            <FontAwesomeIcon icon={faStrikethrough} />
+          </button>
           <button type="button" onMouseDown={(e) => this.handleBlockStyle(e, 'ordered-list-item')}>
             <FontAwesomeIcon icon={faListOl} />
           </button>
           <button type="button" onMouseDown={(e) => this.handleBlockStyle(e, 'unordered-list-item')}>
             <FontAwesomeIcon icon={faListUl} />
           </button>
+          <button type="button" onMouseDown={(e) => this.handleAlginStyle(e, 'ALIGN-LEFT')}>
+            <FontAwesomeIcon icon={faAlignLeft} />
+          </button>
+          <button type="button" onMouseDown={(e) => this.handleAlginStyle(e, 'ALIGN-CENTER')}>
+            <FontAwesomeIcon icon={faAlignCenter} />
+          </button>
+          <button type="button" onMouseDown={(e) => this.handleAlginStyle(e, 'ALIGN-RIGHT')}>
+            <FontAwesomeIcon icon={faAlignRight} />
+          </button>
+          <button type="button" onMouseDown={(e) => this.handleAlginStyle(e, 'ALIGN-JUSTIFY')}>
+            <FontAwesomeIcon icon={faAlignJustify} />
+          </button>
         </div>
         <Editor
           ref={(content) => { this.editor = content; }}
           placeholder="Enter some text"
+          blockStyleFn={blockStyleFn}
+          customStyleMap={customStyleMap}
           editorState={editorState}
           onChange={this.handleChange}
         />
